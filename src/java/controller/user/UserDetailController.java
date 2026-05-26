b07af5b82dcaf256ac6ef0c890f65d8beba664f2
SKtbifakerno1vipro <vuphu12k12@gmail.com>
2026-05-26 10:30:45 +0700

    fix not respone user detail

--- src/java/controller/user/UserDetailController.java
+++ src/java/controller/user/UserDetailController.java
@@ -1,19 +1,22 @@
 package controller.user;
+
 import service.UserService;
-import model.User;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.annotation.WebServlet;
+
 @WebServlet(name = "UserDetailController", urlPatterns = {"/user-detail"})
 public class UserDetailController extends HttpServlet {
+
     private final UserService userService = new UserService();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String id = request.getParameter("id");
-        request.setAttribute("user", userService.getUserById(Integer.parseInt(id)));
+        request.setAttribute("u", userService.getUserById(Integer.parseInt(id)));
         request.getRequestDispatcher("/views/user/detail.jsp").forward(request, response);
     }
 }
