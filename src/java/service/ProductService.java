cd4250eff8e4caa9c2c8e6e30071bed1c62bf757
kiennguyenba2005 <kiennguyenba2005@gmail.com>
2026-05-31 19:24:07 +0700

    nguyenkien 3105

--- src/java/service/ProductService.java
+++ src/java/service/ProductService.java
@@ -6,6 +6,7 @@ import model.Category;
 import model.Product;
 
 public class ProductService {
+
     private final ProductDAO productDAO = new ProductDAO();
 
     public List<Product> getAllProducts() {
@@ -15,32 +16,58 @@ public class ProductService {
     public Product getProductById(int id) {
         return productDAO.getProductById(id);
     }
-    
-    public boolean createProduct(Product product){
+
+    public boolean createProduct(Product product) {
         return productDAO.createProduct(product);
     }
-    
-    public boolean updateProduct(Product product){
+
+    public boolean updateProduct(Product product) {
         return productDAO.updateProduct(product);
     }
-    
-    public List<Product> searchProduct(String searchText, Integer categoryId, String status){
+
+    public List<Product> searchProduct(String searchText, Integer categoryId, String status) {
         return productDAO.searchProduct(searchText, categoryId, status);
     }
-    
-    public List<Category> getAllCategory(){
+
+    public List<Product> searchProduct(String searchText, Integer categoryId, String status, int pageSize, int totalRow, int page, int totalPage) {
+        return productDAO.searchProduct(searchText, categoryId, status, pageSize, totalRow, page, totalPage);
+    }
+
+    public List<Category> getAllCategory() {
         return productDAO.getAllCategory();
     }
-    
-    public List<String> getProductUnit(){
+
+    public List<String> getProductUnit() {
         return productDAO.getProductUnit();
     }
-    
-    public List<String> getProductStatus(){
+
+    public List<String> getProductStatus() {
         return productDAO.getProductStatus();
     }
-    
-    public String getUpdateByWithProductId(int id){
+
+    public String getUpdateByWithProductId(int id) {
         return productDAO.getUpdateByWithProductId(id);
+    }
+
+    public int countProduct(String searchText, Integer categoryId, String status) {
+        return productDAO.countProduct(searchText, categoryId, status);
+    }
+
+    public int calculateTotalPage(int totalRow, int pageSize) {
+        
+        if (pageSize <= 0) {
+            return 1;
+        }
+        return (int) Math.ceil((double) totalRow / pageSize);
+    }
+    
+    public int nomalizePage(int page, int totalPage){
+        if(page < 1){
+            return 1;
+        }
+        if(page > totalPage){
+            return totalPage;
+        }
+        return page;
     }
 }
