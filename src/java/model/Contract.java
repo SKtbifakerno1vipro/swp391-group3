6edfd82d6f3bf5645533c83ce7d2bdae438f3074
SKtbifakerno1vipro <vuphu12k12@gmail.com>
2026-06-06 18:32:02 +0700

    upload dao, contract

---
+++ src/java/model/Contract.java
@@ -0,0 +1,79 @@
+
+
+package model;
+
+import java.time.LocalDateTime;
+
+public class Contract {
+private int contractId,customerId,quotationId;
+private String contractNumber, contractFileUrl,contractStatus;
+private LocalDateTime signAt, createAt;
+
+    public int getContractId() {
+        return contractId;
+    }
+
+    public void setContractId(int contractId) {
+        this.contractId = contractId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getQuotationId() {
+        return quotationId;
+    }
+
+    public void setQuotationId(int quotationId) {
+        this.quotationId = quotationId;
+    }
+
+    public String getContractNumber() {
+        return contractNumber;
+    }
+
+    public void setContractNumber(String contractNumber) {
+        this.contractNumber = contractNumber;
+    }
+
+    public String getContractFileUrl() {
+        return contractFileUrl;
+    }
+
+    public void setContractFileUrl(String contractFileUrl) {
+        this.contractFileUrl = contractFileUrl;
+    }
+
+    public String getContractStatus() {
+        return contractStatus;
+    }
+
+    public void setContractStatus(String contractStatus) {
+        this.contractStatus = contractStatus;
+    }
+
+    public LocalDateTime getSignAt() {
+        return signAt;
+    }
+
+    public void setSignAt(LocalDateTime signAt) {
+        this.signAt = signAt;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public Contract() {
+    }
+
+}
