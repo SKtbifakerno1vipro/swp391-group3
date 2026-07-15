2c87b4000feca419ada7a586205a30f28a54e135
Giangdz102 <gianghy2005@gmail.com>
2026-07-15 22:48:49 +0700

    fix bug orderdal

--- src/java/dal/CustomerOrderDAO.java
+++ src/java/dal/CustomerOrderDAO.java
@@ -9,6 +9,8 @@ import model.Customer;
 import model.User;
 import dto.CustomerOrderDTO;
 import java.sql.Timestamp;
+import model.CustomerOrderDetail;
+import model.Product;
 
 public class CustomerOrderDAO extends DBContext {
 
@@ -62,7 +64,7 @@ public class CustomerOrderDAO extends DBContext {
             ps.setInt(1, orderId);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                model.CustomerOrderDetail cod = new model.CustomerOrderDetail();
+                CustomerOrderDetail cod = new CustomerOrderDetail();
                 cod.setCustomerOrderDetailId(rs.getInt("customer_order_detail_id"));
                 cod.setCustomerOrderId(rs.getInt("customer_order_id"));
                 cod.setQuotationDetailId(rs.getInt("quotation_detail_id"));
@@ -72,13 +74,13 @@ public class CustomerOrderDAO extends DBContext {
                 cod.setTaxPercent(rs.getDouble("tax_percent"));
                 cod.setDiscountPercent(rs.getDouble("discount_percent"));
 
-                model.Product p = new model.Product();
+                Product p = new Product();
                 p.setProductId(rs.getInt("product_id"));
                 p.setProductName(rs.getString("product_name"));
                 p.setSellingPrice(rs.getDouble("selling_price"));
                 p.setUnit(rs.getString("unit"));
 
-                dto.CustomerOrderDTO detailDto = new dto.CustomerOrderDTO();
+                CustomerOrderDTO detailDto = new CustomerOrderDTO();
                 detailDto.setDetail(cod);
                 detailDto.setProduct(p);
                 details.add(detailDto);
